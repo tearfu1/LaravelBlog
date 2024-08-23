f41fed3d8c2721877c28721b5d34d668f208ddb1
tearfu1 <axuior12@gmail.com>
2024-08-23 13:34:56 +0200

    added admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', IndexController::class)->name('main.index');
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix'=>'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', IndexController::class);
+    });
 });
 
 Auth::routes();
